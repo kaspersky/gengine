41d2f69126d89cab3ecd7ed549094c63b605453e
Vladimir Cernov <cernov.vladimir@gmail.com>
2018-05-20 18:45:09 -0700

    Consider the cache in cache mcts

--- src/mcts/mcts_cache.hpp
+++ src/mcts/mcts_cache.hpp
@@ -16,8 +16,26 @@ struct GameData
 };
 
 template <typename IGame>
+struct GameHash
+{
+    std::size_t operator()(IGame *game) const
+    {
+        return game->Hash();
+    }
+};
+
+template <typename IGame>
+struct GameEqual
+{
+    bool operator()(IGame *game1, IGame *game2) const
+    {
+        return *game1 == *game2;
+    }
+};
+
+template <typename IGame>
 void
-MCTS_cache_(std::unordered_map<IGame *, GameData *> &cache, const IGame &root_game, GameData *game_data)
+MCTS_cache_(std::unordered_map<IGame *, GameData *, GameHash<IGame>, GameEqual<IGame>> &cache, const IGame &root_game, GameData *game_data)
 {
     IGame rg(root_game);
     game::IPlayer player_to_move = rg.GetPlayerToMove();
@@ -63,13 +81,34 @@ MCTS_cache_(std::unordered_map<IGame *, GameData *> &cache, const IGame &root_ga
     }
 
     auto moves = rg.GetPossibleMoves();
+    std::unordered_map<IGame *, game::IMove, GameHash<IGame>, GameEqual<IGame>> positions;
     for (auto m : moves)
     {
         auto new_game = new IGame(rg);
-        auto new_node = new GameData;
         new_game->ApplyMove(m);
-        node->children.emplace_back(m, new_node);
-        cache[new_game] = new_node;
+        if (positions.find(new_game) != positions.end())
+        {
+            delete new_game;
+            continue;
+        }
+        positions[new_game] = m;
+    }
+    for (auto p : positions)
+    {
+        GameData *new_node = nullptr;
+        auto it = cache.find(p.first);
+        if (it == cache.end())
+        {
+            new_node = new GameData;
+            cache[p.first] = new_node;
+        }
+        else
+        {
+            new_node = it->second;
+            delete p.first;
+        }
+
+        node->children.emplace_back(p.second, new_node);
 
         nodes.push_back(new_node);
 
@@ -103,7 +142,7 @@ template <typename IGame>
 std::vector<std::pair<game::IMove, std::pair<double, long long>>>
 MCTS_cache(const IGame &game, long long iterations)
 {
-    std::unordered_map<IGame *, GameData *> cache;
+    std::unordered_map<IGame *, GameData *, GameHash<IGame>, GameEqual<IGame>> cache;
     auto game_copy = new IGame(game);
     auto game_data = new GameData;
     cache[game_copy] = game_data;
