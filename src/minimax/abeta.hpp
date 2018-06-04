cb785a658aae8a7e88ffbb043bf1dfd0a044ab16
Vladimir Cernov <cernov.vladimir@gmail.com>
2018-06-03 21:33:06 -0700

    Add multithreading to ABeta

--- src/minimax/abeta.hpp
+++ src/minimax/abeta.hpp
@@ -47,14 +47,37 @@ ABeta<IGame, Eval>::operator()(const IGame &game, int depth) const
 {
     if (depth <= 0)
         return {};
-    std::vector<std::pair<game::IMove, double>> results;
+
     auto moves = game.GetPossibleMoves();
-    for (auto m : moves)
-    {
-        IGame new_game(game);
-        new_game.ApplyMove(m);
-        results.emplace_back(m, -ABeta_<IGame, Eval>(new_game, depth - 1, -std::numeric_limits<double>::max(), std::numeric_limits<double>::max()));
+    std::vector<std::future<std::vector<std::pair<game::IMove, double>>>> futures;
+    int num_threads = std::thread::hardware_concurrency();
+
+    std::vector<std::vector<game::IMove>> workloads(num_threads);
+    for (unsigned i = 0; i < moves.size(); ++i)
+        workloads[i % num_threads].push_back(moves[i]);
+
+    for (int i = 0; i < num_threads; ++i) {
+        const auto &workload = workloads[i];
+        futures.emplace_back(std::async(std::launch::async, [game, depth, &workload] {
+            std::vector<std::pair<game::IMove, double>> results;
+            for (auto m : workload)
+            {
+                IGame new_game(game);
+                new_game.ApplyMove(m);
+                results.emplace_back(m, -ABeta_<IGame, Eval>(new_game, depth - 1, -std::numeric_limits<double>::max(), std::numeric_limits<double>::max()));
+            }
+            return results;
+        }));
     }
+
+    std::vector<std::pair<game::IMove, double>> results;
+    for (auto &f : futures)
+    {
+        auto result = f.get();
+        for (const auto &r : result)
+            results.emplace_back(r.first, r.second);
+    }
+
     std::sort(std::begin(results), std::end(results), [](const std::pair<game::IMove, double> &e1, const std::pair<game::IMove, double> &e2) {
         return e1.second > e2.second;
     });
